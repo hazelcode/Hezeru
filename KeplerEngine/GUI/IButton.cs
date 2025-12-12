a1e8d9c06b0836229065c2cc7894cf79bd149b5e
Hazel Rojas <lacosaqhabla@gmail.com>
2025-12-12 00:45:57 -0600

    Declare initial button interface

--- KeplerEngine/GUI/IButton.cs
+++ KeplerEngine/GUI/IButton.cs
@@ -1,6 +1,20 @@
+using System;
+using Microsoft.Xna.Framework;
+
 namespace KeplerEngine.GUI;
 
 public interface IButton
 {
-    
+    public int X { get; }
+    public int Y { get; }
+    public Rectangle Bounds { get; }
+    public bool Hidden { get; set; }
+    public bool IsHovered { get; set; }
+    public bool IsClicked { get; set; }
+    public string Text { get; set; }
+    public Action OnClick { get; set; }
+    public Action OnRelease { get; set; }
+    public ElementAnchorData AnchorData { get; set; }
+    public void Update(GameTime gameTime);
+    public void Draw(GameTime gameTime);
 }
