78f4e6702385d3003616fcff54a3d45dbbbcc883
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2025-12-14 18:50:59 +0000

    Implement TextureButton

--- KeplerEngine/GUI/TextureButton.cs
+++ KeplerEngine/GUI/TextureButton.cs
@@ -2,27 +2,41 @@ using System.Drawing;
 using KeplerEngine.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Color = Microsoft.Xna.Framework.Color;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
 
 namespace KeplerEngine.GUI;
 
 public class TextureButton : BaseButton
 {
     private TextureAtlas _textureAtlas;
-    private SpriteFont _textFont;
+    private byte _state;
 
-    public TextureButton(string text, Texture2D texture, Size buttonSize, SpriteFont textFont) : base(text)
+    public TextureButton(Texture2D texture, Size buttonTextureSize, Size buttonSize) : base("")
     {
-        _textureAtlas = new TextureAtlas(texture, buttonSize.Width, buttonSize.Height, 1, 3);
-        _textFont = textFont;
+        _textureAtlas = new TextureAtlas(texture, buttonTextureSize.Width, buttonTextureSize.Height, 1, 3);
+        Bounds = new Rectangle(0, 0, buttonSize.Width, buttonSize.Height);
     }
 
     public override void Update(GameTime gameTime)
     {
         base.Update(gameTime);
+
+        if (IsClicked)
+            _state = 2;
+        else if (IsHovered)
+            _state = 1;
+        else
+            _state = 0;
     }
 
     public override void Draw(GameTime gameTime)
     {
-        throw new System.NotImplementedException();
+        Globals.SpriteBatch.Draw(
+            _textureAtlas.Texture,
+            Bounds,
+            _textureAtlas.GetTileBounds(_state),
+            Color.White
+        );
     }
 }
