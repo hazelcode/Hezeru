304a1215b653254498204af9d88a5e8216edfe40
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2025-12-14 20:11:03 +0000

    Resize play button and change Y offset

--- Hezeru/UI/MainMenuPlayButton.cs
+++ Hezeru/UI/MainMenuPlayButton.cs
@@ -6,9 +6,9 @@ namespace Hezeru.UI;
 
 public class MainMenuPlayButton : TextureButton
 {
-    public MainMenuPlayButton(Texture2D texture) : base(texture, new Size(48, 16), new Size(96, 32))
+    public MainMenuPlayButton(Texture2D texture) : base(texture, new Size(48, 16), new Size(48 * 8, 16 * 8))
     {
-        AnchorData = new ElementAnchorData(ElementAnchor.TopCenter, 0, 106);
+        AnchorData = new ElementAnchorData(ElementAnchor.TopCenter, 0, 126);
         OnClick = () => { };
         OnRelease = () => { };
     }
