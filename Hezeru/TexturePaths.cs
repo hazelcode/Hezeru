c9aff3233a8fef85598267d7d1edf4753e99c42f
Hazel Rojas <lacosaqhabla@gmail.com>
2025-12-21 14:39:59 -0600

    Define World Terrain Atlas in Texture paths

--- Hezeru/TexturePaths.cs
+++ Hezeru/TexturePaths.cs
@@ -7,6 +7,8 @@ public static class TexturePaths
     public const string HEZERU_LOGO = "Sprites/HezeruLogo";
 
     public const string MOUSE_POINTER = "Sprites/MousePointer";
-    
+
     public const string MAIN_MENU_PLAY_BUTTON = "Sprites/UI/PlayButton";
+
+    public const string WORLD_TERRAIN_ATLAS = "Atlases/WorldTerrain";
 }
