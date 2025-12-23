6a7c2d368c63b07e4f6ed8bf8f53e3ee79f25707
Hazel Rojas <lacosaqhabla@gmail.com>
2025-12-23 00:41:45 -0600

    Remove unnecessary using statements in StartupLoader.cs

--- Hezeru/Loading/StartupLoader.cs
+++ Hezeru/Loading/StartupLoader.cs
@@ -1,9 +1,5 @@
-using System;
 using System.Collections.Generic;
-using Hezeru.Scenes;
 using KeplerEngine;
-using KeplerEngine.Graphics;
-using KeplerEngine.GUI;
 using KeplerEngine.MemoryCaching;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
