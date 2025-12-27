ac96870ccf8bc9740129ad5ec6ca808c48f208c2
Hazel Rojas <lacosaqhabla@gmail.com>
2025-12-26 22:21:01 -0600

    Define enumeration for collisionless tiles

---
+++ Hezeru/World/CollisionlessTiles.cs
@@ -0,0 +1,15 @@
+namespace Hezeru.World;
+
+public enum CollisionlessTiles : sbyte
+{
+    None = -1,
+    #region Portals
+    BluePortal = 0,
+    GreenPortal = 1,
+    #endregion
+    #region Leaves
+    DryLeaves = 2,
+    DryWitheredLeaves = 3,
+    GreenLeaves = 4
+    #endregion
+}
