4d2be2d27a3a01a1ce2882d9bb98ce1f2800fe08
Hazel Rojas <lacosaqhabla@gmail.com>
2025-12-26 23:07:32 -0600

    Add Tiled project and Main map test

---
+++ Hezeru/Content/Maps/CollisionlessTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="CollisionlessTiles" tilewidth="16" tileheight="16" tilecount="6" columns="3">
+ <image source="../Atlases/WorldCollisionlessTiles.png" trans="ff00ff" width="48" height="32"/>
+</tileset>
